7abd632576a28f02484e9aa722ac79122e4e47d3
Verbal27 <43434781+Verbal27@users.noreply.github.com>
2025-09-09 02:27:27 +0300

    Changes to Dockerfile

--- start.sh
+++ start.sh
@@ -2,5 +2,8 @@
 # Inject PG config for Railway Postgres
 echo "$PG_SERVICE_CONF" > /home/appuser/.pg_service.conf
 
+python manage.py makemigrations
+python manage.py migrate
+
 # Start Gunicorn on Railway dynamic port
 exec gunicorn split_bill.wsgi:application --bind 0.0.0.0:$PORT
