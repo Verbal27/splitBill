fb07da3217c18d746c8a395c90e50ab6f6d316bd
Verbal27 <43434781+Verbal27@users.noreply.github.com>
2025-09-09 13:54:50 +0300

    Changed mailing settings

--- split_bill/settings.py
+++ split_bill/settings.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from decouple import config
 from datetime import timedelta
 import dj_database_url
 import os
@@ -89,11 +88,12 @@ SIMPLE_JWT = {
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = config("EMAIL_HOST_USER", cast=str)
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", cast=str)
-EMAIL_PORT = 587
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.gmail.com")
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 
 LOGIN_REDIRECT_URL = "apps/api/login/"
 
