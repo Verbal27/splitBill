93013829e0dfee96ef1d59a936840263c0b625e7
Verbal27 <43434781+Verbal27@users.noreply.github.com>
2025-09-18 13:26:13 +0300

    Fixed migrations issue

--- start.sh
+++ start.sh
@@ -2,8 +2,7 @@
 # Inject PG config for Railway Postgres
 echo "$PG_SERVICE_CONF" > /home/appuser/.pg_service.conf
 
-python manage.py makemigrations
-python manage.py migrate
+python manage.py migrate --noinput
 
 # Start Gunicorn on Railway dynamic port
 exec gunicorn split_bill.wsgi:application --bind 0.0.0.0:$PORT
